b4223f6dca9d19a88321c8f1032f102c6936fc0c
dhorning <dhorning@eastwood.com>
2018-12-18 22:10:08 -0500

    MASTER: adding interface bootstrap code to code base

---
+++ includes/footer.php
@@ -0,0 +1,13 @@
+        <hr>
+            <footer>
+                <div class="row">
+                    <div class="col-lg-12">
+                        <p>Copyright &copy; Your Website 2014</p>
+                    </div>
+                </div>
+            </footer>
+        </div>
+        <script src="js/jquery.js"></script>
+        <script src="js/bootstrap.min.js"></script>
+    </body>
+</html>
--- index.php
+++ index.php
@@ -1,6 +1,11 @@
-<?php 
-
-echo 'initial index file';
-
-?>
-
+<?php include("includes/header.php"); ?>
+    <div class="row">
+        <div class="col-md-8">
+            <?php 
+                echo 'initial index file';
+            ?> 
+        </div>
+        <div class="col-md-4">
+            <?php include("includes/sidebar.php"); ?>
+        </div>
+        <?php include("includes/footer.php"); ?>
